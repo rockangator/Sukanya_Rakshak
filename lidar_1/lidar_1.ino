3eabc12c46290f6be4b13c4acfb6e20d376e40a1
rockangator <32011655+rockangator@users.noreply.github.com>
2018-10-26 18:33:53 +0530

    new files

---
+++ lidar_1/lidar_1.ino
@@ -0,0 +1,58 @@
+/* this program is the interpretation routine of standard output protocol of TFmini product on Arduino.
+For details, refer to Product Specifications.
+For Arduino boards with only one serial port like UNO board, the function of software visual serial port is
+to be used.
+*/
+
+#include<SoftwareSerial.h>//header file of software serial port
+//#define rxPin 2
+//#define txPin 3
+//SoftwareSerial rt(rxPin,txPin); //define software serial port name as rt and define pin2 as RX and pin3 as TX
+SoftwareSerial rt(2,3); 
+
+/* For Arduinoboards with multiple serial ports like DUEboard, interpret above two pieces of code and
+directly use rt serial port*/
+
+int dist;//actual distance measurements of LiDAR
+int strength;//signal strength of LiDAR
+int check;//save check value
+int i;
+int uart[9];//save data measured by LiDAR
+const int HEADER=0x59;//frame header of data package
+
+void setup()
+{
+Serial.begin(9600);//set bit rate of serial port connecting Arduino with computer
+rt.begin(115200);//set bit rate of serial port connecting LiDAR with Arduino
+}
+
+void loop()
+{
+if (rt.available())//check if serial port has data input
+{
+if(rt.read()==HEADER)//assess data package frame header 0x59
+{
+uart[0]=HEADER;
+if(rt.read()==HEADER)//assess data package frame header 0x59
+{
+uart[1]=HEADER;
+for(i=2;i<9;i++)//save data in array
+{
+uart[i]=rt.read();
+}
+check=uart[0]+uart[1]+uart[2]+uart[3]+uart[4]+uart[5]+uart[6]+uart[7];
+if(uart[8]==(check&0xff))//verify the received data as per protocol
+{
+dist=uart[2]+uart[3]*256;//calculate distance value
+strength=uart[4]+uart[5]*256;//calculate signal strength value
+Serial.print("dist = ");
+Serial.print(dist);//output measure distance value of LiDAR
+Serial.print('\t');
+Serial.print("strength = ");
+Serial.print(strength);//output signal strength value
+Serial.print('\n');
+}
+}
+}
+}
+}
---
+++ lidar_2/lidar_2.ino
@@ -0,0 +1,25 @@
+#include <DFRobot_TFmini.h>
+
+SoftwareSerial mySerial(12, 13); // RX, TX
+
+DFRobot_TFmini  TFmini;
+uint16_t distance,strength;
+
+void setup(){
+    Serial.begin(115200);
+    TFmini.begin(mySerial);
+}
+
+void loop(){
+    if(TFmini.measure()){                      //Measure Distance and get signal strength
+        distance = TFmini.getDistance();       //Get distance data
+        strength = TFmini.getStrength();       //Get signal strength data
+        Serial.print("Distance = ");
+        Serial.print(distance);
+        Serial.println("cm");
+        Serial.print("Strength = ");
+        Serial.println(strength);
+        delay(500);
+    }
+    delay(500);
+}
